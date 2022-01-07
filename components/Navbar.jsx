7764079f2133e2aa207ad9ef28401bdd5fe2d8ee
Bodi Augustus <bodiAugustus@protonmail.com>
2022-01-07 13:32:00 -0600

    Installed npm react-icons package. Imported two react icons for the open/close menu bar. Created a photos folder inside of public folder. Brought in a logo and imported it to the Navbar component after compressing. Installed npm ethers package for blockchain manipulation. Imported useRouter inside the Navbar and used it to push to sprucey.dev when the logo is clicked.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,27 @@
+import {GiAbstract092,GiAbstract093} from "react-icons/gi"
+import logo from '../public/images/logo.png'
+import Image from 'next/image'
+import { Router, useRouter } from "next/router"
+
 const Navbar = () => {
+    const sprucey = "https://www.sprucey.dev"
+    const router = useRouter()
     return (
-        <h1 >Navbar</h1>
+        <nav className="w-full flex md:justify-center justify-between items-center">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+               <div className="ml-2 mt-2">
+               <Image 
+                    src="/images/logo.png"
+                    alt="logo"
+                    height={45}
+                    width={45}
+                    layout="fixed"
+                    className="rounded-lg cursor-crosshair"
+                    onClick={() => router.push(sprucey)}
+                />
+               </div>
+            </div>
+        </nav>
     )
 }
 
