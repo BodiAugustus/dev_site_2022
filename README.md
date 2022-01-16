dd0f3a683608004229dadf67ac7517437d08903d
Bodi Augustus <bodiAugustus@protonmail.com>
2022-01-16 16:15:23 -0600

    Set up UI message to display when user is on wrong network. Created .env dev and prod files for default chain id. Created targetNetwork variable.

--- README.md
+++ README.md
@@ -115,3 +115,5 @@ Commits
 #57. Worked to display actual network to UI. Used swr inside of the useNetwork file to fetch the network id. Created a useEffect to update changes to the network in the UI. 
 
 #58. Small UI fix - during network change hexadecimal is displayed before numeric value for network id. Used partInt on the mutated value to fix. Created object of network id's with maps to string to display names instead of id's to the UI. 
+
+#59. Set up UI message to display when user is on wrong network. Created .env dev and prod files for default chain id. Created targetNetwork variable. 
--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -13,8 +13,10 @@ const NETWORKS = {
     1337: "Ganache"
 }
 
+const targetNetwork =  NETWORKS[process.env.NEXT_PUBLIC_TARGET_CHAIN_ID]
+
 export const handler = (web3, provider) => () => {
-    const {mutate, ...rest} = useSWR(() => 
+    const {data, mutate, ...rest} = useSWR(() => 
         web3 ? "web3/network" : null,
         async () => {
             const chainId = await web3.eth.getChainId()
@@ -28,7 +30,10 @@ export const handler = (web3, provider) => () => {
     },[web3])
     return {
         network: {
+            data,
             mutate,
+            target: targetNetwork,
+            isSupported: data === targetNetwork,
             ...rest
         }
     }
