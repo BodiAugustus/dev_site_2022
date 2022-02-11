bd5c6bf16ac2e138aea91ec40ddc2b9f11f139bb
Bodi Augustus <bodiAugustus@protonmail.com>
2022-02-11 16:16:41 -0600

    Update Footer.jsx

--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -10,9 +10,7 @@ const Footer = () => {
     return (
         <footer className="h-[34vh] flex flex-col justify-center items-center text-2xl text-white bg-sky-900 pt-6 pb-2">
         <Link href="/" className="transition-all">
-
           <a className="neontext neonP font-cinzel text-4xl sm:text-4xl md:text-7xl lg:text-8xl cursor-default lg:mt-4 pb-10">Bodi Agustus</a>
-
         </Link>
         <div className=" w-full text-center translate-y-5 md:translate-y-16 lg:translate-y-2  ">
         {
